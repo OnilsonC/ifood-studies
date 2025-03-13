d6a32017c4437a083617a935551c0f6d24cef061
onilson-csf <onilson.cardoso@gmail.com>
2025-03-13 17:15:34 -0300

    first commit

---
+++ marketplace/src/test/java/br/com/onilson/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onilson.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
